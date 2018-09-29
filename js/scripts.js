bbd5269e5347220237cd92f104d516604804e7c6
Raphael Katana <roduor41@gmail.com>
2018-09-29 21:27:29 +0300

    add functionality for player 1

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,29 @@ $(document).ready(function(){
     event.preventDefault();
     var tally=roll()
 
+// Player 1 functionality
+    function p1(){
+      score1.innerHTML=gameRound;
+      tally1.innerHTML=gameRound+run;
+      score2.innerHTML=0;
+      tally2.innerHTML=0;
+      $("#hold").click(function(event){
+        event.preventDefault();
+        tally=run*2;
+
+        tally1.innerHTML=tally;
+      })
+
+      if(run>1){
+        p2();
+      }
+      else if(tally>=50||tally1.innerHTML>=50){
+        alert("Great Job; Player 1 wins")
+        results.innerHTML=("Great Job; Player 1 wins")
+      }
+      else{
+        p1();
+      }
+    }
+
     
