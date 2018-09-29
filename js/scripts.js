255817afd8d4e4a5c1f2abdac9effbc174407ff0
Raphael Katana <roduor41@gmail.com>
2018-09-29 21:24:58 +0300

    create user interface

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,11 @@ function roll() {
   var run = (Math.floor(Math.random()*2)+1);
   return(gameRound+=run);
 }
+// User interface
+$(document).ready(function(){
+  alert("Start Game!")
+  $("#run").click(function(event){
+    event.preventDefault();
+    var tally=roll()
+
+    
